d79522aa67d22f3e64153a65cef4fd0ce05459de
PH68960 <dinhtienduc1010@gmail.com>
2025-11-28 11:27:04 +0700

    Thông báo

--- CODE_Tai_Lop/Program.c
+++ CODE_Tai_Lop/Program.c
@@ -1,24 +1,68 @@
-// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
 
 #include <stdio.h>
+#include <math.h>
+void kiemTraSoNguyen()
+{
+    printf("kiem tra so nguyen\n");
+}
+void lapChucNang(int chonChucNang)
+{
+    int tiepTuc = 1;
+    while (tiepTuc == 1)
+    {
+        switch (chonChucNang)
+        {
+            case 1:
+                kiemTraSoNguyen();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                printf("Chon lai [0-10]\n");
+
+        }
+        printf("Tiep tuc chuc nang nay? [1=Co | 0=Khong\n");
+        scanf_s("%d", &tiepTuc);
+        system("cls"); // clear screen
+    }
+}
+
 
 int main()
 {
-    printf("Hi there!\n");
+    int chonChucNang;
+    do
+    {
+        printf("-------MENU-----------\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("2. Uoc chung - Boi Chung");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("1. Kiem Tra So Nguyen");
+        printf("\n");
+        printf("Moi chon chuc nang [1-10]: ");
+        scanf_s("%d", &chonChucNang);
+        if(chonChucNang != 0) {
+            lapChucNang(chonChucNang);
+        }
+        
+    } while (chonChucNang != 0);
+    
 }
 
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Dung "Solution Explorer window" de them/quan ly cac files
-//   2. Dung "Output window" de kiem tra "build output" va "cac thong bao khac"
-//   3. Dung "Error List window" de xem loi
-//   4. Chon "Project > Add New Item" de tao moi file code,
-//      hoac "Project > Add Existing Item" de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//      chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//      bam phim chuot phai vao "Solution" > chon "Open Folder in File Explorer"
-
-// GV: AnhTT184
