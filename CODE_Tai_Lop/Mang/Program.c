ee8962a88f62c2c9c25ffd674de165969b307e3f
PH68960 <dinhtienduc1010@gmail.com>
2025-12-01 10:46:01 +0700

    Cap Nhap

--- CODE_Tai_Lop/Mang/Program.c
+++ CODE_Tai_Lop/Mang/Program.c
@@ -6,14 +6,18 @@ int main()
 {
 		int n;
 		int y[10];
+		int tong = 0;
 		printf("Nhap phan tu: ");
 		scanf_s("%d", &n);
 		for (int i = 0; i < n; i++) {
 			printf("y[%d] = ", i);
 			scanf_s("%d", &y[i]);
 		}
-		printf("Cac phan tu cua mang la: \n");
+		printf("Cac phan tu cua mang la: ");
 		for (int i = 0; i < n; i++) {
 			printf(" %d", y[i]);
+			tong += y[i];
 		}
+		printf("\n");
+		printf("Tong cac so la: %d ", tong);
 }
---
+++ Form menu/Program.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void lapChucNang(int chon)
+{
+    int tiepTuc = 1;
+    while (tiepTuc == 1)
+    {
+        switch (chon)
+        {
+        case 1:
+            printf("Ban da chon chuc nang 1\n");
+            break;
+        case 2:
+            printf("Ban da chon chuc nang 2\n");
+            break;
+        case 3:
+            printf("Ban da chon chuc nang 3\n");
+            break;
+        case 4:
+            printf("Ban da chon chuc nang 4\n");
+            break;
+        default:
+            printf("Hay chon lai tu cn [1-10]: \n");
+            break;
+        }
+
+        printf("Tiep tuc chuc nang nay? [1=Co | 0=Khong]: ");
+        scanf_s("%d", &tiepTuc);
+
+        system("cls");
+    }
+}
+int main()
+{
+    int chon;
+    do {
+        printf("=========MENU=========\n");
+        printf("1. \n");
+        printf("2. \n");
+        printf("3. \n");
+        printf("Thoat chuong trinh: [0]\n");
+        printf("Chon chuc nang:");
+        printf("\n");
+        scanf_s("%d", &chon);
+        if (chon != 0) {
+            lapChucNang(chon);
+        }
+    } while (chon != 0);
+}
